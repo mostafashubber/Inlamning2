ee9fcac9a675f04fafe2948d4266ce1b033d7ec0
Mostafa Shubber <m_shubber@hotmail.com>
2020-12-18 13:47:19 +0100

    Skapade en class som heter Basgrupp

---
+++ Inlamning2/Basgrupp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inlamning2
+{
+     public  class Basgrupp
+    {
+        public int Födelsedatum { get; set; }
+        public string Namn { get; set; }
+        public string Födelseort { get; set; }
+        public string Land { get; set; }
+        public string Stad { get; set; }
+        public int Längd { get; set; }
+        public string Civilstatus { get; set; }
+        public string intresse { get; set; }
+        public string Favoritmat { get; set; }
+        public string Motivation { get; set; }
+    }
+}
